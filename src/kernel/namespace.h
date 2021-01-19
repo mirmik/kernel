5f0aed83a26f1efb3fd1905b39384cec13236fd3
mirmik <mirmikns@yandex.ru>
2021-01-19 16:37:24 +0300

    'a'

--- src/kernel/namespace.h
+++ src/kernel/namespace.h
@@ -1,9 +1,24 @@
 #ifndef KERNEL_NAMESPACE_H
 #define KERNEL_NAMESPACE_H
 
+struct directory_pointer 
+{
+	struct namespace * ns;
+	const char * path;
+};
+
+struct resource_pointer 
+{
+	struct namespace * ns;
+	const char * path;
+};
+
 struct namespace_operations 
 {
 	void (*lookup)(const char * path, resource *);
+	void (*iterate)(
+		struct resource_pointer * iter, 
+		struct directory_pointer * dir);
 };
 
 struct namespace 
